2caab8d20619a10a7847ff3ed3a56ba51a164473
David Brieden <davidbrieden@aol.com>
2023-07-04 16:52:40 +0200

    fixed whitespace

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,23 +3,23 @@ return {
   {"vim-scripts/ClosePairs"}, -- automaticly insert closing brackets
   {
     "vim-airline/vim-airline",
-	init = function()
-	  vim.g.airline_powerline_fonts = true
-	  vim.g.airline_theme= "papercolor"
-	  vim.g["airline#extensions#tabline#enabled"] = true
-	end
+    init = function()
+      vim.g.airline_powerline_fonts = true
+      vim.g.airline_theme= "papercolor"
+      vim.g["airline#extensions#tabline#enabled"] = true
+    end
   }, -- status/tabline
   {"vim-airline/vim-airline-themes"},
   {"NLKNguyen/papercolor-theme"},
   {
     "scrooloose/nerdtree",
-	keys = {
-	  {"<F9>", ":NERDTreeToggle<cr>"}
-	},
-	init = function()
+    keys = {
+      {"<F9>", ":NERDTreeToggle<cr>"}
+    },
+    init = function()
       vim.g.NERDTreeIgnore = {"node_modules", ".git"}
       vim.g.NERDTreeShowHidden=1
-	end
+    end
   }, -- file tree
   {"xuyuanp/nerdtree-git-plugin"},
   {"tpope/vim-fugitive"}, -- git plugin
@@ -27,24 +27,24 @@ return {
   {"airblade/vim-gitgutter"}, -- shows git changes
   {
     "phaazon/hop.nvim",
-	config = true,
-	keys = {
-	  {"f", ":HopChar1MW<cr>"},
-	  {"/", ":HopPatternMW<cr>"},
-	  {"<leader>w", ":HopWordMW<cr>"},
-	}
+    config = true,
+    keys = {
+      {"f", ":HopChar1MW<cr>"},
+      {"/", ":HopPatternMW<cr>"},
+      {"<leader>w", ":HopWordMW<cr>"},
+    }
   }, -- shows git changes
   {
     "majutsushi/tagbar",
-	keys = {
-	  {"<F8>", ":TagbarToggle<cr>"}
-	}
+    keys = {
+      {"<F8>", ":TagbarToggle<cr>"}
+    }
   },
   {
     "preservim/nerdcommenter",
     init = function()
       vim.g.NERDSpaceDelims = true
-	  vim.g.NERDDefaultAlign = 'left'
+      vim.g.NERDDefaultAlign = 'left'
     end
   }, -- commenting stuff
   {"mhinz/vim-startify"},
@@ -57,23 +57,29 @@ return {
         { "\"",    mode = { "n", "v" } },
         { "<C-R>", mode = "i" }
     },
+    opts = {
+      show_empty = false,
+      window = {
+        border = "double"
+      }
+    },
     cmd = "Registers",
-	config = true
-	--config = function()
-	--	require("registers").setup()
-	--end,
+    config = true
+    --config = function()
+    --    require("registers").setup()
+    --end,
   },
-  
+
   -- file finder
   {"nvim-lua/plenary.nvim"}, -- dependency of telescope
   {
-	"nvim-telescope/telescope.nvim",
-	init = function()
+    "nvim-telescope/telescope.nvim",
+    init = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
-  }, 
+  },
 }
