40de0799665d9414e85679695644515d5ddb8555
David Brieden <davidbrieden@aol.com>
2023-07-04 18:02:43 +0200

    moved telescope config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -72,14 +72,4 @@ return {
 
   -- file finder
   {"nvim-lua/plenary.nvim"}, -- dependency of telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    init = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
-  },
 }
