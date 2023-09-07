6894788080945e929e682b4dcb89cbaae995282f
David Brieden <davidbrieden@aol.com>
2023-09-07 14:04:36 +0200

    added telescope extension for emojis

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -14,13 +14,21 @@ return {
     init = function()
       local builtin = require('telescope.builtin')
       local wk = require("which-key")
+      local keymap_set = vim.keymap.set
 
       wk.register({["<leader>f"] = {name = "+Telescope"}})
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc= "Find Files"})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc= "Live Grep"})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc= "Find Buffers"})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc= "Find Help Tags"})
-      vim.keymap.set('n', '<leader>fn', find_nvim, {desc= "Find Nvim Config Files"})
+      keymap_set('n', '<leader>ff', builtin.find_files, {desc= "Find Files"})
+      keymap_set('n', '<leader>fg', builtin.live_grep, {desc= "Live Grep"})
+      keymap_set('n', '<leader>fb', builtin.buffers, {desc= "Find Buffers"})
+      keymap_set('n', '<leader>fh', builtin.help_tags, {desc= "Find Help Tags"})
+      keymap_set('n', '<leader>fn', find_nvim, {desc= "Find Nvim Config Files"})
+      keymap_set('n', '<leader>fe', require("telescope").extensions.emoji.emoji, {desc= "Find Nvim Config Files"})
     end
   },
+  {
+    "xiyaowong/telescope-emoji.nvim",
+    init = function()
+      require("telescope").load_extension("emoji")
+    end
+  }
 }
