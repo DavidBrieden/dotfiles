e387b18b2743933ff3f39260d895168207083b59
David <davidbrieden@aol.com>
2023-11-16 07:28:13 +0100

    added insert mode navigation keymaps

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -17,6 +17,13 @@ keymap("n", "<leader>ev", ":split $MYVIMRC<cr>", default_opts("Open vimrc"))
 -- folding
 keymap("n", "<leader><leader>", "za", default_opts("Toggle fold under cursor"))
 
+-- marks
 keymap("n", "<C-F2>", "m;", {})
 keymap("n", "<F2>", "m]", {})
 keymap("n", "<S-F2>", "m[", {})
+
+-- insert mode navigation
+keymap("i", "<C-h>", "<Left>", default_opts("move left"))
+keymap("i", "<C-j>", "<Down>", default_opts("move down"))
+keymap("i", "<C-k>", "<Up>", default_opts("move up"))
+keymap("i", "<C-l>", "<Right>", default_opts("move right"))
